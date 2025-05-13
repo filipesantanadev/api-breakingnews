97bc4c77b23585c3fab41041c784383ce416fcbc
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-05-13 04:02:34 -0300

    Add comments

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+//ROTA
+    // Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+        // GET - Pega um info
+        // POST - Cria uma info
+        // PUT - Altera toda a info
+        // PATCH - Altera parte da info
+        // DELETE - Apaga uma info
+
+// Name - Um identificador da rota
+
+// Function (Callback) - Responsável por executar algum comando
+
+
+app.get('/soma', (req, res) => {
+    const soma = 1 + 1;
+
+    res.send({soma})
 })
 
 app.listen(3000)
