272d3311ff5b2591aba4e1a656836068f3eb7fa4
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-05-16 10:39:28 -0300

    refactor: add try catch in controller and middlewares

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-// Importa o módulo Express, que é um framework para criar servidores web em Node.js
 const express = require("express");
-// Cria uma instância do aplicativo Express
-const app = express();
-// Conecta ao banco de dados MongoDB usando a função definida em db.js
 const connectDatabase = require("./src/database/db");
-
-// Importa as rotas relacionadas ao usuário, definidas em outro arquivo
 const userRoute = require("./src/routes/user.route");
 
-// Define a porta em que o servidor irá rodar
 const port = 3000;
+const app = express();
 
-connectDatabase(); // Chama a função para conectar ao banco de dados
-
-// Configura o Express para aceitar requisições com corpo em formato JSON
+connectDatabase();
 app.use(express.json());
-
-// Usa as rotas de usuário para qualquer requisição que comece com "/user"
 app.use("/user", userRoute);
 
-// Inicia o servidor e exibe uma mensagem no console quando estiver rodando
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
-
-//*****ESTUDO*******
-//ROTA
-// Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-// GET - Pega um info
-// POST - Cria uma info
-// PUT - Altera toda a info
-// PATCH - Altera parte da info
-// DELETE - Apaga uma info
-
-// Name - Um identificador da rota
-
-// Function (Callback) - Responsável por executar algum comando
--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -1,29 +1,39 @@
 const mongoose = require("mongoose");
 const userService = require("../services/user.service");
 
+// Middleware to validate if the ID is a valid MongoDB ObjectId
 const validId = (req, res, next) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: "Invalid user ID" });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({ message: "Invalid user ID" });
+    }
+
+    next();
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  next();
 };
 
+// Middleware to check if the user exists in the database
 const validUser = async (req, res, next) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  const user = await userService.findByIdService(id);
+    const user = await userService.findByIdService(id);
 
-  if (!user) {
-    return res.status(400).send({ message: "User not found" });
+    if (!user) {
+      return res.status(400).send({ message: "User not found" });
+    }
+
+    req.id = id;
+    req.user = user;
+
+    next();
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  req.id = id;
-  req.user = user;
-
-  next();
 };
 
 module.exports = { validId, validUser };
