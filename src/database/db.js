f75f64b719d57198b4ae6cd4fe74b8ceb044bab6
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-05-18 17:44:02 -0300

    refactor: add bcrypt and dotenv

--- src/database/db.js
+++ src/database/db.js
@@ -4,10 +4,10 @@ const connectDatabase = () => {
   console.log("Wait connecting to the database...");
 
   mongoose
-    .connect(
-      "mongodb+srv://root:proot@cluster0.kuion1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB Atlas connected"))
     .catch((error) => console.log("MongoDB Atlas connection error: ", error));
 };
