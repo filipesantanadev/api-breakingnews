58a86a8a33aa80426455cdcfd8f39ed77e09adbf
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-05-22 19:46:27 -0300

    feat: add search by title and news by user

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -4,6 +4,8 @@ import {
   countNews,
   topNewsService,
   findByIdService,
+  searchByTitleService,
+  byUserService,
 } from "../services/news.service.js";
 
 const create = async (req, res) => {
@@ -140,4 +142,58 @@ const findById = async (req, res) => {
   }
 };
 
-export { create, findAll, topNews, findById };
+const searchByTitle = async (req, res) => {
+  try {
+    const { title } = req.query;
+
+    const news = await searchByTitleService(title);
+
+    if (news.lenght === 0) {
+      return res
+        .status(400)
+        .send({ message: "There are no news with this title" });
+    }
+
+    return res.send({
+      //countNews: news.length,
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+const byUser = async (req, res) => {
+  try {
+    const id = req.userId;
+    const news = await byUserService(id);
+
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export { create, findAll, topNews, findById, searchByTitle, byUser };
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -11,10 +11,22 @@ const topNewsService = () => News.findOne().sort({ _id: -1 }).populate("user");
 
 const findByIdService = (id) => News.findById(id).populate("user");
 
+const searchByTitleService = (title) =>
+  News.find({
+    title: { $regex: `${title || ""}`, $options: "i" },
+  })
+    .sort({ _id: -1 })
+    .populate("user");
+
+const byUserService = (id) =>
+  News.find({ user: id }).sort({ _id: -1 }).populate("user");
+
 export {
   createService,
   findAllService,
   countNews,
   topNewsService,
   findByIdService,
+  searchByTitleService,
+  byUserService,
 };
