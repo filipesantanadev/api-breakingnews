151117cb744bfa9bba2521fafc8bd30033e4b1f9
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-05-22 16:13:54 -0300

    refactor: add try catch in get all news on controller

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -28,54 +28,60 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req, res) => {
-  let { limit, offset } = req.query;
+  try {
+    let { limit, offset } = req.query;
 
-  limit = Number(limit);
-  offset = Number(offset);
+    limit = Number(limit);
+    offset = Number(offset);
 
-  if (!limit) {
-    limit = 5;
+    if (!limit) {
+      limit = 5;
+    }
+
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
+    if (news.length === 0) {
+      return res.status(400).send({ message: "There are no registered news" });
+    }
+
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  if (!offset) {
-    offset = 0;
-  }
-
-  const news = await findAllService(offset, limit);
-  const total = await countNews();
-  const currentUrl = req.baseUrl;
-
-  const next = offset + limit;
-  const nextUrl =
-    next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
-
-  const previous = offset - limit < 0 ? null : offset - limit;
-  const previousUrl =
-    previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
-
-  if (news.length === 0) {
-    return res.status(400).send({ message: "There are no registered news" });
-  }
-
-  res.send({
-    nextUrl,
-    previousUrl,
-    limit,
-    offset,
-    total,
-
-    results: news.map((item) => ({
-      id: item._id,
-      title: item.title,
-      text: item.text,
-      banner: item.banner,
-      likes: item.likes,
-      comments: item.comments,
-      name: item.user.name,
-      username: item.user.username,
-      userAvatar: item.user.avatar,
-    })),
-  });
 };
 
 export { create, findAll };
